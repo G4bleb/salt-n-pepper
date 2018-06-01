964576ccaad9a9f3b11c8e327a3509b7cdcf58f7
lologit1998 <loresnb@yahoo.com>
2018-06-01 13:49:18 +0200

    add image

---
+++ website/php/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+    <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="../css/bootstrap-theme.min.css" rel="stylesheet" type="text/css" />
+    <link href="../css/header.css" rel="stylesheet" type="text/css">
+
+  </head>
+  <body>
+    <div class="rect_bleu">
+      <img src="../img/burger.png" class="img_burger"></img>
+    </div>
+
+
+    <script src="../js/bootstrap.min.js"></script>
+  </body>
+
+
+</html>
