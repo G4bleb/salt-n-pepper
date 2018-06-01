4eff8aa74b2e67ef536591a536806e1c45c52ec6
lologit1998 <loresnb@yahoo.com>
2018-06-01 14:01:19 +0200

    more bootstrap

--- website/php/header.php
+++ website/php/header.php
@@ -7,11 +7,15 @@
     <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <link href="../css/bootstrap-theme.min.css" rel="stylesheet" type="text/css" />
     <link href="../css/header.css" rel="stylesheet" type="text/css">
+    <link href="../fonts/"
 
   </head>
   <body>
     <div class="rect_bleu">
+      <a href="../index.php"><span class="glyphicons glyphicons-home"></span></a>
       <img src="../img/burger.png" class="img_burger"></img>
+      <button type="button" class="btn btn-default">Connexion</button>
+      <button type="button" class="btn btn-default">Inscription</button>
     </div>
 
 
