1a039e898f6c25b305da8b1c56c00ee5ae71b865
lologit1998 <loresnb@yahoo.com>
2018-06-01 16:28:41 +0200

    header update

---
+++ website/register.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Salt'n'Pepper - Inscription</title>
+
+    <?php
+    include 'php/header.php';
+    ?>
+    <?php
+    include 'php/footer.php';
+     ?>
+
+</html>
