239628d79a0ed6c5facd04a5f7eda07ff072dcc2
lologit1998 <loresnb@yahoo.com>
2018-06-05 17:34:59 +0200

    finished user

--- mainmenu.cpp
+++ mainmenu.cpp
@@ -7,28 +7,20 @@ MainMenu::MainMenu(Driver *driver,Connection *con,QWidget *parent) :
 {
     ui->setupUi(this);
     menuConnexion=parent;
+    this->con_user = con;
+    this->driver_user=driver;
 
-    stmt_show_user=con->createStatement();
+
+     ui->pushButton_modify_user->setEnabled(false);
+     ui->lineEdit_high_score->setEnabled(false);
+     ui->lineEdit_login->setEnabled(false);
+
+    stmt_show_user=con_user->createStatement();
     res_show_user=stmt_show_user->executeQuery("SELECT id_user,login,best_score FROM user");
-    row_table=k=0;
 
+    row_table=0;
     column_table=ui->tableWidget_User->columnCount();
 
-     /*while(res_show_user->next()){
-        vecteurLabel.append(new QLabel(res_show_user->getString(1).c_str()));
-        vecteurLabel.append(new QLabel(res_show_user->getString(2).c_str()));
-        vecteurLabel.append(new QLabel(res_show_user->getString(3).c_str()));
-        row_table++;
-    }
-
-    ui->tableWidget_User->setRowCount(row_table);
-
-    for(int i=0;i<row_table;i++){
-        for(int j=0;j<column_table;j++){
-        ui->tableWidget_User->setCellWidget(i,j,vecteurLabel[k]);
-        k++;
-       }
-    }*/
     while(res_show_user->next()){
         QVector <QLabel*> vecteurLabel;
         vecteurLabel.append(new QLabel(res_show_user->getString(1).c_str()));
@@ -37,14 +29,19 @@ MainMenu::MainMenu(Driver *driver,Connection *con,QWidget *parent) :
         vecteurLabels.append(vecteurLabel);
         row_table++;
     }
-    ui->tableWidget_User->setRowCount(row_table);
+
+    ui->tableWidget_User->setRowCount(row_table); //create lines
+
     for(int i=0;i<row_table;i++){
         for(int j=0;j<column_table;j++){
         ui->tableWidget_User->setCellWidget(i,j,vecteurLabels[i][j]);
-        k++;
         qDebug()<<vecteurLabels[i][j]->text();
        }
     }
+
+    delete stmt_show_user;
+    delete res_show_user;
+
 }
 
 MainMenu::~MainMenu()
@@ -69,6 +66,45 @@ void MainMenu::on_pushButton_disconnect_clicked()
 
 void MainMenu::on_tableWidget_User_cellClicked(int row, int column)
 {
-    if(column=1) ui->lineEdit_login->setText(vecteurLabels[row][column]->text());
-    if(column=2) ui->lineEdit_high_score->setText(vecteurLabels[row][column]->text());
+    selected_row=row;
+    ui->lineEdit_login->setText(vecteurLabels[row][1]->text());
+    ui->lineEdit_high_score->setText(vecteurLabels[row][2]->text());
+
+    ui->pushButton_modify_user->setEnabled(true);
+    ui->lineEdit_high_score->setEnabled(true);
+    ui->lineEdit_login->setEnabled(true);
+}
+
+
+void MainMenu::on_pushButton_modify_user_clicked()
+{
+    stmt_modify_user=con_user->createStatement();
+    cout << "coucou" << endl;
+
+    if((ui->lineEdit_pwd->text())!=NULL){
+        cout << "coucou2" << endl;
+        stmt_modify_user->executeUpdate("UPDATE user set login='"+ui->lineEdit_login->text().toStdString()+"', best_score="+ui->lineEdit_high_score->text().toStdString()+", password=sha2('"+ui->lineEdit_pwd->text().toStdString()+"',256) where id_user="+vecteurLabels[selected_row][0]->text().toStdString()+";");
+    }
+    else{
+        cout << "coucou3" << endl;
+        stmt_modify_user->executeUpdate("UPDATE user set login='"+ui->lineEdit_login->text().toStdString()+"', best_score="+ui->lineEdit_high_score->text().toStdString()+" where id_user="+vecteurLabels[selected_row][0]->text().toStdString()+";");
+    }
+
+    delete stmt_modify_user;
+
+    this->hide();
+    MainMenu* pageuser=new MainMenu(this->driver_user,this->con_user,menuConnexion);
+    pageuser->show();
+}
+
+void MainMenu::on_pushButton_delete_user_clicked()
+{
+    stmt_delete_user=con_user->createStatement();
+    stmt_delete_user->executeUpdate("DELETE from user where id_user="+vecteurLabels[selected_row][0]->text().toStdString()+";");
+
+    delete stmt_delete_user;
+
+    this->hide();
+    MainMenu* pageuser=new MainMenu(this->driver_user,this->con_user,menuConnexion);
+    pageuser->show();
 }
--- mainmenu.hpp
+++ mainmenu.hpp
@@ -22,7 +22,6 @@ class MainMenu;
 class MainMenu : public QDialog
 {
     Q_OBJECT
-
 public:
     explicit MainMenu(Driver * driver,Connection * con, QWidget *parent = 0);
     ~MainMenu();
@@ -31,18 +30,21 @@ private slots:
     void on_pushButton_select_clicked();
     void on_pushButton_disconnect_clicked();
     void on_tableWidget_User_cellClicked(int row, int column);
+    void on_pushButton_modify_user_clicked();
+    void on_pushButton_delete_user_clicked();
 
 private:
     Ui::MainMenu *ui;
     QuestionsMenu *questionsMenu;
     QWidget * menuConnexion;
-    Connection * con;
+    Connection * con_user;
+    Driver * driver_user;
     Statement * stmt_show_user;
+    Statement * stmt_delete_user;
+    Statement * stmt_modify_user;
     ResultSet * res_show_user;
-    QVector <QLabel*> vecteurLabel;
-
     QVector <QVector <QLabel*> > vecteurLabels;
-    int row_table,column_table,k;
+    int row_table,column_table,selected_row;
 };
 
 #endif // MAINMENU_HPP
