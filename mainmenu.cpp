aa46763994696694116c0fd3eaf4b7c6e572178c
lologit1998 <loresnb@yahoo.com>
2018-06-06 12:23:47 +0200

    second thumbnail

--- mainmenu.cpp
+++ mainmenu.cpp
@@ -7,40 +7,75 @@ MainMenu::MainMenu(Driver *driver,Connection *con,QWidget *parent) :
 {
     ui->setupUi(this);
     menuConnexion=parent;
-    this->con_user = con;
-    this->driver_user=driver;
 
+    this->driver_first_window=driver;
+    this->con_first_window = con;
 
-     ui->pushButton_modify_user->setEnabled(false);
-     ui->lineEdit_high_score->setEnabled(false);
-     ui->lineEdit_login->setEnabled(false);
+    ui->pushButton_set_user->setEnabled(false);
+    ui->pushButton_delete_user->setEnabled(false);
+    ui->lineEdit_high_score->setEnabled(false);
+    ui->lineEdit_login->setEnabled(false);
+    ui->lineEdit_pwd->setEnabled(false);
 
-    stmt_show_user=con_user->createStatement();
+    ui->pushButton_delete_topic->setEnabled(false);
+    ui->pushButton_set_topic->setEnabled(false);
+    ui->pushButton_look_topic->setEnabled(false);
+
+    stmt_show_user=con_first_window->createStatement();
     res_show_user=stmt_show_user->executeQuery("SELECT id_user,login,best_score FROM user");
 
-    row_table=0;
-    column_table=ui->tableWidget_User->columnCount();
+    stmt_show_topic=con_first_window->createStatement();
+    res_show_topic=stmt_show_topic->executeQuery("SELECT id_topic,topic_name FROM topic");
+
+    row_table_user=0;
+    column_table_user=ui->tableWidget_User->columnCount();
 
     while(res_show_user->next()){
         QVector <QLabel*> vecteurLabel;
         vecteurLabel.append(new QLabel(res_show_user->getString(1).c_str()));
         vecteurLabel.append(new QLabel(res_show_user->getString(2).c_str()));
         vecteurLabel.append(new QLabel(res_show_user->getString(3).c_str()));
-        vecteurLabels.append(vecteurLabel);
-        row_table++;
+        TableFirstThumbnail.append(vecteurLabel);
+        row_table_user++;
     }
 
-    ui->tableWidget_User->setRowCount(row_table); //create lines
+    ui->tableWidget_User->setRowCount(row_table_user); //create lines
 
-    for(int i=0;i<row_table;i++){
-        for(int j=0;j<column_table;j++){
-        ui->tableWidget_User->setCellWidget(i,j,vecteurLabels[i][j]);
-        qDebug()<<vecteurLabels[i][j]->text();
+    for(int i=0;i<row_table_user;i++){
+        for(int j=0;j<column_table_user;j++){
+        ui->tableWidget_User->setCellWidget(i,j,TableFirstThumbnail[i][j]);
+       // qDebug()<<TableFirstThumbnail[i][j]->text();
+       }
+    }
+
+
+
+
+    row_table_topic=0;
+    column_table_topic=ui->tableWidget_Topic->columnCount();
+
+    while(res_show_topic->next()){
+        QVector <QLabel*> vecteurLabel;
+        vecteurLabel.append(new QLabel(res_show_topic->getString(1).c_str()));
+        vecteurLabel.append(new QLabel(res_show_topic->getString(2).c_str()));
+        TableSecondThumbnail.append(vecteurLabel);
+        row_table_topic++;
+    }
+
+    ui->tableWidget_Topic->setRowCount(row_table_topic); //create lines
+
+    for(int i=0;i<row_table_topic;i++){
+        for(int j=0;j<column_table_topic;j++){
+        ui->tableWidget_Topic->setCellWidget(i,j,TableSecondThumbnail[i][j]);
+        //qDebug()<<TableSecondThumbnail[i][j]->text();
        }
     }
 
     delete stmt_show_user;
     delete res_show_user;
+
+    delete stmt_show_topic;
+    delete res_show_topic;
 
 }
 
@@ -49,12 +84,12 @@ MainMenu::~MainMenu()
     delete ui;
 }
 
-void MainMenu::on_pushButton_select_clicked()
+/*void MainMenu::on_pushButton_select_clicked()
 {
     this->hide();
     questionsMenu = new QuestionsMenu(this);
     questionsMenu->show();
-}
+}*/
 
 
 void MainMenu::on_pushButton_disconnect_clicked()
@@ -66,45 +101,84 @@ void MainMenu::on_pushButton_disconnect_clicked()
 
 void MainMenu::on_tableWidget_User_cellClicked(int row, int column)
 {
-    selected_row=row;
-    ui->lineEdit_login->setText(vecteurLabels[row][1]->text());
-    ui->lineEdit_high_score->setText(vecteurLabels[row][2]->text());
+    selected_row_user=row;
+    ui->lineEdit_login->setText(TableFirstThumbnail[row][1]->text());
+    ui->lineEdit_high_score->setText(TableFirstThumbnail[row][2]->text());
 
-    ui->pushButton_modify_user->setEnabled(true);
+    ui->pushButton_set_user->setEnabled(true);
+    ui->pushButton_delete_user->setEnabled(true);
     ui->lineEdit_high_score->setEnabled(true);
     ui->lineEdit_login->setEnabled(true);
+    ui->lineEdit_pwd->setEnabled(true);
 }
 
 
-void MainMenu::on_pushButton_modify_user_clicked()
+void MainMenu::on_pushButton_set_user_clicked()
 {
-    stmt_modify_user=con_user->createStatement();
-    cout << "coucou" << endl;
+    stmt_modify_user=con_first_window->createStatement();
 
-    if((ui->lineEdit_pwd->text())!=NULL){
-        cout << "coucou2" << endl;
-        stmt_modify_user->executeUpdate("UPDATE user set login='"+ui->lineEdit_login->text().toStdString()+"', best_score="+ui->lineEdit_high_score->text().toStdString()+", password=sha2('"+ui->lineEdit_pwd->text().toStdString()+"',256) where id_user="+vecteurLabels[selected_row][0]->text().toStdString()+";");
-    }
-    else{
-        cout << "coucou3" << endl;
-        stmt_modify_user->executeUpdate("UPDATE user set login='"+ui->lineEdit_login->text().toStdString()+"', best_score="+ui->lineEdit_high_score->text().toStdString()+" where id_user="+vecteurLabels[selected_row][0]->text().toStdString()+";");
-    }
+    if((ui->lineEdit_pwd->text())!=NULL)stmt_modify_user->executeUpdate("UPDATE user set login='"+ui->lineEdit_login->text().toStdString()+"', best_score="+ui->lineEdit_high_score->text().toStdString()+", password=sha2('"+ui->lineEdit_pwd->text().toStdString()+"',256) where id_user="+TableFirstThumbnail[selected_row_user][0]->text().toStdString()+";");
+
+    else stmt_modify_user->executeUpdate("UPDATE user set login='"+ui->lineEdit_login->text().toStdString()+"', best_score="+ui->lineEdit_high_score->text().toStdString()+" where id_user="+TableFirstThumbnail[selected_row_user][0]->text().toStdString()+";");
 
     delete stmt_modify_user;
 
     this->hide();
-    MainMenu* pageuser=new MainMenu(this->driver_user,this->con_user,menuConnexion);
+    MainMenu* pageuser=new MainMenu(this->driver_first_window,this->con_first_window,menuConnexion);
     pageuser->show();
 }
 
 void MainMenu::on_pushButton_delete_user_clicked()
 {
-    stmt_delete_user=con_user->createStatement();
-    stmt_delete_user->executeUpdate("DELETE from user where id_user="+vecteurLabels[selected_row][0]->text().toStdString()+";");
-
+    stmt_delete_user=con_first_window->createStatement();
+    stmt_delete_user->executeUpdate("DELETE from user where id_user="+TableFirstThumbnail[selected_row_user][0]->text().toStdString()+";");
     delete stmt_delete_user;
 
     this->hide();
-    MainMenu* pageuser=new MainMenu(this->driver_user,this->con_user,menuConnexion);
+    MainMenu* pageuser=new MainMenu(this->driver_first_window,this->con_first_window,menuConnexion);
+    pageuser->show();
+}
+
+void MainMenu::on_tableWidget_Topic_cellClicked(int row, int column)
+{
+    selected_row_topic=row;
+    ui->lineEdit_topic->setText(TableSecondThumbnail[row][1]->text());
+
+    ui->pushButton_delete_topic->setEnabled(true);
+    ui->pushButton_set_topic->setEnabled(true);
+    ui->pushButton_look_topic->setEnabled(true);
+
+}
+
+void MainMenu::on_pushButton_set_topic_clicked()
+{
+    stmt_modify_topic=con_first_window->createStatement();
+    stmt_modify_topic->executeUpdate("UPDATE topic set topic_name='"+ui->lineEdit_topic->text().toStdString()+"' where id_topic="+TableSecondThumbnail[selected_row_topic][0]->text().toStdString()+";");
+    delete stmt_modify_topic;
+
+    this->hide();
+    MainMenu* pageuser=new MainMenu(this->driver_first_window,this->con_first_window,menuConnexion);
+    pageuser->show();
+}
+
+void MainMenu::on_pushButton_delete_topic_clicked()
+{
+    stmt_delete_topic=con_first_window->createStatement();
+    stmt_delete_topic->executeUpdate("DELETE from topic where id_topic="+TableSecondThumbnail[selected_row_topic][0]->text().toStdString()+";");
+    delete stmt_delete_topic;
+
+    this->hide();
+    MainMenu* pageuser=new MainMenu(this->driver_first_window,this->con_first_window,menuConnexion);
+    pageuser->show();
+}
+
+void MainMenu::on_pushButton_add_topic_clicked()
+{
+    stmt_add_topic=con_first_window->createStatement();
+    stmt_add_topic->executeUpdate("INSERT INTO topic (id_topic,topic_name) values (NULL,'"+ui->lineEdit_topic->text().toStdString()+"');");
+    delete stmt_add_topic;
+
+    this->hide();
+    MainMenu* pageuser=new MainMenu(this->driver_first_window,this->con_first_window,menuConnexion);
     pageuser->show();
 }
--- mainmenu.hpp
+++ mainmenu.hpp
@@ -27,24 +27,54 @@ public:
     ~MainMenu();
 
 private slots:
-    void on_pushButton_select_clicked();
-    void on_pushButton_disconnect_clicked();
+
+    /*---------- method USER ----------*/
     void on_tableWidget_User_cellClicked(int row, int column);
-    void on_pushButton_modify_user_clicked();
+    void on_pushButton_set_user_clicked();
     void on_pushButton_delete_user_clicked();
+
+    /*---------- method TOPIC ----------*/
+    void on_tableWidget_Topic_cellClicked(int row, int column);
+    void on_pushButton_set_topic_clicked();
+
+    /*---------- other method ----------*/
+    void on_pushButton_disconnect_clicked();
+
+
+    void on_pushButton_delete_topic_clicked();
+
+    void on_pushButton_add_topic_clicked();
 
 private:
     Ui::MainMenu *ui;
     QuestionsMenu *questionsMenu;
     QWidget * menuConnexion;
-    Connection * con_user;
-    Driver * driver_user;
+
+
+    /*--------- DRIVER ----------*/
+    Driver * driver_first_window;
+
+    /*--------- CONNECTION ----------*/
+    Connection * con_first_window;
+
+    /*--------- STATEMENT ----------*/
     Statement * stmt_show_user;
     Statement * stmt_delete_user;
     Statement * stmt_modify_user;
+
+    Statement * stmt_show_topic;
+    Statement * stmt_delete_topic;
+    Statement * stmt_modify_topic;
+    Statement * stmt_add_topic;
+
+    /*--------- RESULTEST ----------*/
     ResultSet * res_show_user;
-    QVector <QVector <QLabel*> > vecteurLabels;
-    int row_table,column_table,selected_row;
+    ResultSet * res_show_topic;
+
+    QVector <QVector <QLabel*> > TableFirstThumbnail;
+    QVector <QVector <QLabel*> > TableSecondThumbnail;
+
+    int row_table_user,column_table_user,row_table_topic,column_table_topic,selected_row_user,selected_row_topic;
 };
 
 #endif // MAINMENU_HPP
